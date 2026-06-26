691ee0375368f990333d58b55264f3d3ee13a2d1
Anuj <anujchauhan2276@gmail.com>
2026-06-26 11:12:01 +0530

    Fixing deployment port and adding health check

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -88,13 +88,13 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # ==========================================
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('NAME'),
-        'USER': os.getenv('USER'),
-        'PASSWORD': os.getenv('PASSWORD'),
-        'HOST': os.getenv('HOST'),
-        'PORT': os.getenv('PORT'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
 
--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse # Import this
+
+# Simple function for the health check
+def home(request):
+    return HttpResponse("Career Clarity API is Live.")
 
 urlpatterns = [
+    path('', home), # Add this line for the root URL
     path('admin/', admin.site.urls),
-    path('', include('roadmaps.urls')), # Connects our roadmap APIs
+    path('', include('roadmaps.urls')),
 ]
